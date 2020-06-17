62be75487c791f89dfebcc48711a0bbb84b5d5a2
王志祥 <1031041547@qq.com>
2020-06-17 14:39:27 +0800

    base_url

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://bzhqy.test/';
+$config['base_url'] = 'http://ee.bzh001.com/';
 
 /*
 |--------------------------------------------------------------------------
